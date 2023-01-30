95beca833cf4388b2e424498a86c426702e2efc2
ricoom <47127588+ricoom@users.noreply.github.com>
2023-01-30 22:03:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let img_selector=document.querySelectorAll('.img-control')
+let img_list=document.querySelectorAll('.image-list')
+let img_slide=document.querySelectorAll('.image-slide')
+let open_btn=document.getElementById('open-btn')
+let close_btn=document.getElementById('close-btn')
+let menu=document.getElementById('menu')
+
+const nextBtn=document.getElementById('next-btn')
+
+function showImage(imageName){
+img_selector.forEach(selector=>{
+    //remove the border
+    selector.classList.remove('active')
+
+    if(selector.innerHTML.includes(imageName)){
+        //add a border for active thumb-nail
+        selector.classList.add('active')
+
+        for (let i = 0; i < img_list.length; i++) {
+            if(img_list[i].innerHTML.includes(imageName)){
+img_list[i].classList.remove('hidden')
+
+
+console.log(i)
+
+        }else{
+            img_list[i].classList.add('hidden')
+
+        }
+            
+        }
+  
+    }
+})
+        
+
+}
+
+      
+ open_btn.addEventListener('click',()=>{
+menu.style.display='block'
+ }) 
+close_btn.addEventListener('click',()=>{
+menu.style.display='none'
+ }) 
+
